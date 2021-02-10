e210e98698856d12523926246a9f835ba3976abb
aravind <aravindsugunan@gmail.com>
2021-02-10 16:02:03 +0530

    adding NEVENTS parmetrization to main.cc

--- DAonGPU/src/main.cc
+++ DAonGPU/src/main.cc
@@ -8,16 +8,17 @@ int main()
 {
 
 	ZTrackSoA * tracksInGPU;
+	const int NEVENTS=20;
 
 	// ZTrackSoA* loadTracksToGPU(std::string csv_fname, int nvts=20,int evtStart=0,int evtEnd=20 );
-	tracksInGPU=loadTracksToGPU("tracks.csv",5);
+	tracksInGPU=loadTracksToGPU("tracks.csv",NEVENTS);
 	
 	if(tracksInGPU == nullptr)
 	 		return 1;
 	
 	gpuDAVertexer::DAVertexer demoVertexer(5.5);
 	
-	for(int i=0;i<1;i++)
+	for(int i=0;i<NEVENTS;i++)
 	{
 	        cout<<"\n@ Doing vertexing for event , "<<i<<"\n";
 		demoVertexer.makeAsync(&tracksInGPU[i],20) ; // the 20 here does not have any meaning , was a prameter passed for testing
