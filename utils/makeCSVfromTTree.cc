ea3527176df47216cbad14b1a3b44f16e4161688
Aravind <aravindsugunan@gmail.com>
2021-04-19 13:15:59 +0200

    Adding the vertex.csv maker to the repo

--- utils/makeCSVfromTTree.cc
+++ utils/makeCSVfromTTree.cc
@@ -1,9 +1,11 @@
-void makeCSVofTracks(TString fname)
+void makeCSVofTracks(TString fname,int N)
 {
 	
 	static constexpr uint32_t MAXTRACKS=60000;
+	static constexpr uint32_t MAXVTX   =50000;
 	TFile file(fname,"READ");
 	auto trackTree = (TTree*) file.Get("getTrackAndVtxData/Track");
+	auto vtxTree = (TTree*) file.Get("getTrackAndVtxData/Vertex");
 	
 	float zTrack[MAXTRACKS];
 	float zErrTrack[MAXTRACKS];
@@ -11,7 +13,10 @@ void makeCSVofTracks(TString fname)
 	float ptTrack[MAXTRACKS];
 	float tipTrack[MAXTRACKS];
 
-	int ntracks;
+	float zVtx[MAXVTX];
+	float dZ2Vtx[MAXVTX];
+	
+	int ntracks,nvtxs;
 
 	trackTree->SetBranchAddress("ntracks",&ntracks);
 	trackTree->SetBranchAddress("zTrack",zTrack);
@@ -19,17 +24,32 @@ void makeCSVofTracks(TString fname)
 	trackTree->SetBranchAddress("ndofTrack",ndofTrack);
 	trackTree->SetBranchAddress("ptTrack",ptTrack);
 	trackTree->SetBranchAddress("tipTrack",tipTrack);
+	
+	vtxTree->SetBranchAddress("nvtxs",&nvtxs);
+	vtxTree->SetBranchAddress("zVertex",zVtx);
+	vtxTree->SetBranchAddress("zErrVertex",dZ2Vtx);
 
-	int N=trackTree->GetEntries();
+	int Na=trackTree->GetEntries();
 
 	fstream csvfile("tracks.csv",ios::out);
 	csvfile<<"#   @evt,numtracks"<<"\n";
 	csvfile<<"#   track_idx,ztrack,zErrTrack,tipTrack,ptTrack,ndofTrack"<<"\n";
+	
+	fstream csvfile_vtx("vertices.csv",ios::out);
+	csvfile_vtx<<"#   @evt,numVtxs"<<"\n";
+	csvfile_vtx<<"#   vtxIdx,zVtx,dZ2Vtx"<<"\n";
+
 
 	for(int i=0;i<N;i++)
 	{
+		if(i%400==0)
+			cout<<"Doing "<<i<<"\n";
 		trackTree->GetEntry(i);
+		vtxTree->GetEntry(i);
+
 		csvfile<<"@"<<i<<","<<ntracks<<"\n";
+		csvfile_vtx<<"@"<<i<<","<<nvtxs<<"\n";
+		
 		for(int j=0;j<ntracks;j++)
 		{
 			csvfile<<j<<",";
@@ -41,8 +61,17 @@ void makeCSVofTracks(TString fname)
 			csvfile<<"\n";
 		}
 	
+         	for(int j=0;j<nvtxs;j++)
+		{
+			csvfile_vtx<<j<<",";
+			csvfile_vtx<<zVtx[j]<<",";
+			csvfile_vtx<<dZ2Vtx[j]<<",";
+			csvfile_vtx<<"\n";
+		}
+	
 	}
 	
 	csvfile.close();
+	csvfile_vtx.close();
 	file.Close();
 }
